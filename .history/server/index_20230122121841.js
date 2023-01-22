1414ebf04a2e7979ccd120582d7f8fc12679b45a
QASEMI JAVAD <javadqasemi24@gmail.com>
2023-01-22 13:36:14 +0100

    app started

---
+++ .history/server/index_20230122121841.js
@@ -0,0 +1,16 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import helmet from "helmet";
+import morgan from "morgan";
+
+/*configuration*/
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginEmbedderPolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(body);
---
+++ .history/server/index_20230122123738.js
@@ -0,0 +1,30 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import helmet from "helmet";
+import morgan from "morgan";
+
+/*Routers import configuration*/
+import clientRouters from "./routers/client.js";
+import generalRouter from "./routers/client.js";
+import clientRouters from "./routers/client.js";
+import clientRouters from "./routers/client.js";
+
+/*configuration*/
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginEmbedderPolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+
+/*router configuration*/
+app.use("/client", clientRouters);
+app.use("/general", generalRouters);
+app.use("management", managementRouters);
+app.use("/sales", salesRouters);
